027310d15ef2933270cd7ef8b3c99aec2b1fa09f
Ashim Gurung <ashimgurung433@gmail.com>
2020-04-26 21:29:09 +0100

    setting variables
    main, investor, Company class

---
+++ src/Investor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ashim
+ */
+public class Investor {
+    
+    public int id;
+    
+    public int budget;
+    
+    public int numSharesPurchased;
+    
+}
