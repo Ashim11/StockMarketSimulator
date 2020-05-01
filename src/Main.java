b627d0ab0ed2fb7ee6b8df1dd6c83c8377065c24
Ashim Gurung <ashimgurung433@gmail.com>
2020-05-01 13:20:09 +0100

    update main and trading class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +16,40 @@
  */
 public class Main {
     
-    public static void main(String[]args){
+        
+        //constant to generate parameters of company and investor
+        private static final int MIN_SHARE_PRICE = 500;
+        private static final int MAX_SHARE_PRICE = 1000;
+        private static final int MIN_BUDGET = 1000;
+        private static final int MAX_BUDGET = 10000;
+        private static final int MIN_NUM_SHARES= 10;
+        private static final int MAX_NUM_SHARES= 100;
+        
+        
+        
+        public static List<Seller> createCompanies(int howMany){
+            List<Seller> companies = new ArrayList<Seller>();
+            for(int i=0; i < howMany; i++){
+                Seller seller = Factory.createSeller(MIN_NUM_SHARES, MAX_NUM_SHARES, MIN_SHARE_PRICE, MAX_SHARE_PRICE);
+                companies.add(seller);
+            }
+            return companies;
+        }
+        
+        public static List<Investor> createInvestors(int howMany){
+            List<Investor> investors = new ArrayList<Investor>();
+            for(int i=0; i < howMany; i++){
+                Investor investor = Factory.createInvestor(MIN_BUDGET, MAX_BUDGET);
+		investors.add(investor);
+            }
+            return investors;
+        }
+        
     
+    public static void main(String[]args){
+        
+        
+
     
     
     
--- src/Trading.java
+++ src/Trading.java
@@ -1,6 +1,7 @@
 
 import java.util.List;
 import java.util.Observable;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,6 +17,8 @@ import java.util.Observable;
 This class wil implements the trading day between a list of companies and 
 investor and after trading simlulation is complete shows report to user with choice. 
 
+This class uses "Observer" behavioral design pattern that notifies each seller whenever 
+10 shares are sold from any company.
 */
 public class Trading extends Observable {
     
@@ -60,15 +63,59 @@ public class Trading extends Observable {
     Execute the simulation of a trading day.
     */
     public void run(){
-    
+        boolean sold = true;
+        
+        while (sold) {
+            sold = false;
+            for (Investor buyer : investors){
+                Seller seller = chooseSellerWithLeastPrice(buyer);
+                if (seller != null){
+                    buyer.buy(seller);
+                    seller.sell();
+                    sold= true;
+                    totalSharesSoldInCurrPhase++;
+                    if(totalSharesSoldInCurrPhase == 10){
+                        setChanged();
+                        notifyObservers();
+                        totalSharesSoldInCurrPhase = 0;
+                    }
+                }
+            
+            }
+        }
+            
+        computerReport();
+        System.out.println("Done");
+        
+        showReports();
     }
     
     /*
+    Helper method for an investor to choose the company with least share price.
+    
+    @param buyer the investor
+    @return the Seller with least share price if any, else return null if investor
+    has no seller has shares left.
     
     */
     private Seller chooseSellerWithLeastPrice(Investor buyer){
     
-        return null;
+        int minPrice = 99999999;
+        Seller minSeller = null;
+        
+        for(Seller seller : companies){
+            if(seller.hasShares() && buyer.hasBudget(seller.getSharePrice())){
+                if(seller.getSharePrice() < minPrice){
+                    minPrice = seller.getSharePrice();
+                    minSeller = seller;
+                
+                }
+            
+            }
+        }
+        
+        
+        return minSeller;
     }
     
     
@@ -76,6 +123,38 @@ public class Trading extends Observable {
     this will compute all the required report 
     */
     private void computerReport(){
+        int minCapital = companies.get(0).getCapital();
+        int maxCapital = minCapital;
+        int minSharesPurchased = investors.get(0).getNumSharesPurchased();
+        int maxSharesPurchased = minSharesPurchased;
+        lowestCapitalCompany = companies.get(0);
+        highestCapitalCompany = companies.get(0);
+        
+        for(Seller seller : companies){
+            if(minCapital > seller.getCapital()){
+                minCapital = seller.getCapital();
+                lowestCapitalCompany = seller;
+                
+            }
+            if(maxCapital < seller.getCapital()){
+                maxCapital = seller.getCapital();
+                highestCapitalCompany = seller;
+                
+            }
+        }
+        
+        for(Investor buyer : investors){
+            if(minSharesPurchased > buyer.getNumSharesPurchased()){
+                minSharesPurchased = buyer.getNumSharesPurchased();
+                lowestSharesPurchasedInvestor = buyer;
+            }
+            if(maxSharesPurchased < buyer.getNumSharesPurchased()){
+                maxSharesPurchased = buyer.getNumSharesPurchased();
+                highestSharesPurchasedInvestor = buyer;
+                
+            }
+        }
+        
     
     }
     
@@ -83,6 +162,45 @@ public class Trading extends Observable {
     wil show interactive reports to user.
     */
     private void showReports(){
+        Scanner input = new Scanner(System.in);
+        int menuChoice = -1;
+        boolean done = false;
+        
+        while(!done){
+            //display report choices menu
+            System.out.println("1. Report company with the highest capital");
+            System.out.println("2. Report company with the lowest capital");
+            System.out.println("3. Report investor with the highest number of shares");
+            System.out.println("4. Report investor with the lowest number of shares");
+            System.out.println("0. Quit");
+            
+            //read choice 
+            System.out.println("\nEnter your choice [0-4]");
+            menuChoice = input.nextInt();
+            switch(menuChoice){
+            case 1:
+                System.out.println(highestCapitalCompany.toString() + "(Capital: " + highestCapitalCompany()+ ")\n");
+                break;
+            case 2:
+                System.out.println(lowestCapitalCompany.toString() + "(Capital: " + lowestCapitalCompany()+ ")\n");
+                break;
+            case 3:
+                System.out.println(highestSharesPurchasedInvestor.toString() + "(#Shares Purchsed: " + highestSharesPurchasedInvestor.getNumSharesPurchased()+ ")\n");
+                break;
+            case 4:
+                System.out.println(lowestSharesPurchasedInvestor.toString() + "(#Shares Purchsed: " + lowestSharesPurchasedInvestor.getNumSharesPurchased()+ ")\n");
+                break;
+            case 0:
+                System.out.println("Report company");
+                break;
+            default:
+                System.out.println("Invalid menu choice! Must be between 0 - 4. \n");
+                break;
+            }
+            
+        
+        }
+        
     
     }
     
